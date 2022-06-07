a01512097b3c4b9dcfdd5f7b2b224939480a1c28
Miklos Suveges <miklos.suveges@gmail.com>
2022-06-07 23:11:22 +0200

    feat(cpp): init with getSwapInput & getSwapOutput DEX-335

---
+++ packages/cpp/src/ConstantProductPool.test.ts
@@ -0,0 +1,111 @@
+import { Fraction, TFractionLike } from "@sundae/fraction";
+import { getSwapOutput, getSwapInput, TPair } from "./ConstantProductPool";
+
+describe("getSwapOutput", () => {
+  const threePct = new Fraction(3, 100);
+  const zeroPct = Fraction.ZERO;
+
+  test("throws if any of the arguments are negative", () => {
+    expect(() => getSwapOutput(-1n, 10n, 10n, Fraction.ZERO)).toThrow();
+    expect(() => getSwapOutput(1n, -10n, 10n, Fraction.ZERO)).toThrow();
+    expect(() => getSwapOutput(1n, 10n, -10n, Fraction.ZERO)).toThrow();
+    expect(() =>
+      getSwapOutput(1n, 1n, 1n, Fraction.asFraction(-0.1))
+    ).toThrow();
+  });
+
+  test("throws if fee is greater than or equal 1", () => {
+    expect(() => getSwapOutput(1n, 10n, 10n, Fraction.asFraction(1))).toThrow();
+    expect(() =>
+      getSwapOutput(1n, 10n, 10n, Fraction.asFraction(1.132))
+    ).toThrow();
+  });
+
+  describe("correct output, next state and fee collected", () => {
+    test.each([
+      [1n, [1n, 1n], zeroPct, 0n, [2n, 1n], 0n],
+      [1n, [1n, 2n], zeroPct, 1n, [2n, 1n], 0n],
+      [100n, [1n, 2n], zeroPct, 1n, [101n, 1n], 0n],
+      [100n, [1n, 100n], zeroPct, 99n, [101n, 1n], 0n],
+      [100n, [100n, 100n], zeroPct, 50n, [200n, 50n], 0n],
+      [50n, [100n, 100n], zeroPct, 33n, [150n, 67n], 0n],
+
+      [1n, [1n, 1n], threePct, 0n, [2n, 1n], 0n],
+      [1n, [1n, 2n], threePct, 0n, [2n, 2n], 0n],
+      [1n, [1n, 3n], threePct, 1n, [2n, 2n], 0n],
+      [100n, [1n, 2n], threePct, 1n, [101n, 1n], 3n],
+      [100n, [1n, 100n], threePct, 98n, [101n, 2n], 3n],
+      [100n, [100n, 100n], threePct, 49n, [200n, 51n], 3n],
+      [50n, [100n, 100n], threePct, 32n, [150n, 68n], 1n],
+    ] as [bigint, TPair, TFractionLike, bigint, TPair, bigint][])(
+      "%# input %d; pool %p; fee %d => output %o; nextPool %p; fee: %d",
+      (input, pool, fee, output, nextPool, lpFee) => {
+        const actual = getSwapOutput(input, ...pool, fee);
+        expect(actual.output).toBe(output);
+        expect(actual.inputLpFee).toBe(lpFee);
+        expect(actual.nextInputReserve).toBe(nextPool[0]);
+        expect(actual.nextInputReserve).toBe(input + pool[0]);
+        expect(actual.nextOutputReserve).toBe(nextPool[1]);
+        expect(actual.nextOutputReserve + actual.output).toBe(pool[1]);
+      }
+    );
+  });
+});
+
+describe("getSwapInput", () => {
+  const threePct = new Fraction(3, 100);
+  const zeroPct = Fraction.ZERO;
+
+  test("throws if any of the arguments are negative", () => {
+    expect(() => getSwapInput(-1n, 10n, 10n, Fraction.ZERO)).toThrow();
+    expect(() => getSwapInput(1n, -10n, 10n, Fraction.ZERO)).toThrow();
+    expect(() => getSwapInput(1n, 10n, -10n, Fraction.ZERO)).toThrow();
+    expect(() =>
+      getSwapInput(1n, 10n, 10n, Fraction.asFraction(-0.1))
+    ).toThrow();
+  });
+
+  test("throws if fee is greater than or equal 1", () => {
+    expect(() => getSwapInput(1n, 10n, 10n, Fraction.asFraction(1))).toThrow();
+    expect(() =>
+      getSwapInput(1n, 10n, 10n, Fraction.asFraction(1.132))
+    ).toThrow();
+  });
+
+  test("throws if output is greater than or equal to output reserve", () => {
+    expect(() =>
+      getSwapInput(10n, 10n, 10n, Fraction.asFraction(0.1))
+    ).toThrow();
+    expect(() => getSwapInput(1n, 1n, 1n, Fraction.asFraction(0.1))).toThrow();
+    expect(() => getSwapInput(10n, 1n, 1n, Fraction.asFraction(0.1))).toThrow();
+  });
+
+  describe("correct input, next state and fee collected", () => {
+    test.each([
+      [1n, [1n, 2n], zeroPct],
+      [100n, [1n, 2n], zeroPct],
+      [100n, [1n, 100n], zeroPct],
+      [100n, [100n, 100n], zeroPct],
+      [50n, [100n, 100n], zeroPct],
+
+      [1n, [1n, 3n], threePct],
+      [100n, [1n, 2n], threePct],
+      [100n, [1n, 100n], threePct],
+      [100n, [100n, 100n], threePct],
+      [50n, [100n, 100n], threePct],
+    ] as [bigint, TPair, TFractionLike][])(
+      "%# input %d; pool %p; fee %d",
+      (input, pool, fee) => {
+        const outcome = getSwapOutput(input, ...pool, fee);
+        const actual = getSwapInput(outcome.output, ...pool, fee);
+        const outcomeForActual = getSwapOutput(actual.input, ...pool, fee);
+        expect(outcome.output).toBe(outcomeForActual.output);
+        expect(actual.inputLpFee).toBe(outcomeForActual.inputLpFee);
+        expect(actual.nextInputReserve).toBe(outcomeForActual.nextInputReserve);
+        expect(actual.nextOutputReserve).toBe(
+          outcomeForActual.nextOutputReserve
+        );
+      }
+    );
+  });
+});
---
+++ packages/cpp/src/ConstantProductPool.ts
@@ -0,0 +1,93 @@
+import { Fraction, TFractionLike } from "@sundae/fraction";
+
+export type TPair = [bigint, bigint];
+// export type TPool = [bigint, bigint, TFractionLike];
+
+export type TSwapOutcome = {
+  input: bigint;
+  output: bigint;
+  inputLpFee: bigint;
+  nextInputReserve: bigint;
+  nextOutputReserve: bigint;
+};
+
+/**
+ * Calculate swap outcome for a given input and pool parameters (input tokens, output tokens, fee).
+ * Throws if
+ *  - any of the arguments are negative
+ *  - fee is greater than or equal 1
+ *
+ * @param input The given amount of tokens to be swapped
+ * @param inputReserve The amount of tokens in the input reserve
+ * @param outputReserve The amount of tokens in the output reserve
+ * @param fractionFee The liquidity provider fee
+ * @returns The swap details
+ */
+export const getSwapOutput = (
+  input: bigint,
+  inputReserve: bigint,
+  outputReserve: bigint,
+  fee: TFractionLike
+): TSwapOutcome => {
+  if (input <= 0 || inputReserve <= 0 || outputReserve <= 0)
+    throw new Error("Input and reserves must be positive");
+
+  fee = Fraction.asFraction(fee);
+  if (fee.lt(Fraction.ZERO) || fee.gte(Fraction.ONE))
+    throw new Error("fee must be [0,1)");
+
+  const feeDiff = fee.denominator - fee.numerator;
+  const output = new Fraction(
+    outputReserve * input * feeDiff,
+    inputReserve * fee.denominator + input * feeDiff
+  ).quotient;
+
+  const inputLpFee = new Fraction(input * fee.numerator, fee.denominator)
+    .quotient;
+  const nextInputReserve = inputReserve + input;
+  const nextOutputReserve = outputReserve - output;
+  return { input, output, inputLpFee, nextInputReserve, nextOutputReserve };
+};
+
+/**
+ * Calculate input required for a swap outcome for a given output and pool parameters (input tokens, output tokens, fee).
+ * Throws if
+ *  - any of the arguments are negative
+ *  - fee is greater than or equal 1
+ *  - output is greater than or equal to output reserve
+ *
+ * @param output
+ * @param inputReserve
+ * @param outputReserve
+ * @param fee
+ * @returns The swap details
+ */
+export const getSwapInput = (
+  output: bigint,
+  inputReserve: bigint,
+  outputReserve: bigint,
+  fee: TFractionLike
+): TSwapOutcome => {
+  if (output <= 0 || inputReserve <= 0 || outputReserve <= 0)
+    throw new Error("Output and reserves must be positive");
+
+  if (output >= outputReserve)
+    throw new Error("Output must be less than output reserve");
+
+  fee = Fraction.asFraction(fee);
+  if (fee.lt(Fraction.ZERO) || fee.gte(Fraction.ONE))
+    throw new Error("fee must be [0,1)");
+
+  const feeDiff = fee.denominator - fee.numerator;
+  const input =
+    new Fraction(
+      inputReserve * output * fee.denominator,
+      (outputReserve - output) * feeDiff
+    ).quotient + 1n;
+
+  const inputLpFee = new Fraction(input * fee.numerator, fee.denominator)
+    .quotient;
+  const nextInputReserve = inputReserve + input;
+  const nextOutputReserve = outputReserve - output;
+  return { input, output, inputLpFee, nextInputReserve, nextOutputReserve };
+};
