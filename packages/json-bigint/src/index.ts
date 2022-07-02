25870aac660f67a5d5797e48485ce2e5ea9ad981
Miklos Suveges <miklos.suveges@gmail.com>
2022-07-02 17:48:36 +0200

    style: lint fix

--- packages/json-bigint/src/index.ts
+++ packages/json-bigint/src/index.ts
@@ -1,13 +1,12 @@
 import JSONBigConstr from "json-bigint";
 
-export const JSONBig = JSONBigConstr
+export const JSONBig = JSONBigConstr;
 
 const JSON = JSONBigConstr({
   useNativeBigInt: true,
   constructorAction: "preserve",
   protoAction: "preserve",
 }) as globalThis.JSON;
-
 
 export const { parse, stringify } = JSON;
 
