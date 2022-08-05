bf513b558392f190497ed65737b3c696dd3ee191
akratzel <andreas.kratzel@gmail.com>
2022-08-05 07:13:08 +0200

    chore: add asset interface

--- packages/asset/src/Asset.ts
+++ packages/asset/src/Asset.ts
@@ -22,3 +22,41 @@ export type TNonFungibleToken = IHasStringId;
 
 export const stringIdEquals = (a: IHasStringId, b: IHasStringId) =>
   a.id === b.id;
+
+export enum ETokenMetadataSource {
+  CardanoTokenRegistry = "CardanoTokenRegistry",
+  OnChainLabel20 = "OnChainLabel20",
+  OnChainLabel721 = "OnChainLabel721",
+}
+
+export interface IAsset {
+  /** hex encoded policyId + hex encoded asset name */
+  assetId: string;
+  /** hex encoded asset name (ledger: asset_name) */
+  assetName?: string;
+  /**
+   * the project's website
+   *  websiteUrl: String
+   * the project's description
+   *  description: String
+   * listed date for token (ISO8601: yyyy-mm-dd)
+   */
+  dateListed: string;
+  /** token decimal places (defaults to 0) */
+  decimals: number;
+  /** description of asset */
+  description?: string;
+  /** logo url or base64 encoded logo */
+  logo?: string;
+  /** market cap of token */
+  marketCap?: string;
+  /** hex encoded policyId, 56 chars length (ledger: policy_id) */
+  policyId: string;
+  /** sources define where the token metadata is sourced from */
+  sources?: ETokenMetadataSource[];
+  /** ticker (max 6-8 characters ?) */
+  ticker?: string;
+  /** total supply of tokens */
+  totalSupply?: string;
+  tvl?: string;
+}
